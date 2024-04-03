db6a1301d46ac21063027f9309c23a723eba2ca2
celsovcsantos <celsovcsantos@gmail.com>
2024-04-03 13:17:22 -0300

    feat: aula2.4

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // Aula2.2
 // func main() {
 // 	numeroDeBytes, erro := fmt.Println("Hello, World", "teste", 100)
@@ -9,15 +7,27 @@ import "fmt"
 // }
 
 // Aula2.3
-var y = "Olá, bom dia!"
+// var y = "Olá, bom dia!"
 
-func main() {
-	x := 10.0
+// func main() {
+// 	x := 10.0
 
-	fmt.Printf("x: %v, %T\n", x, x)
-	fmt.Printf("y: %v, %T\n", y, y)
-	x, z := 20, 40
-	fmt.Printf("x: %v, %T\n", x, x)
-	teste, erro := fmt.Printf("z: %v, %T\n", z, z)
-	fmt.Println(teste, erro)
-}
+// 	fmt.Printf("x: %v, %T\n", x, x)
+// 	fmt.Printf("y: %v, %T\n", y, y)
+// 	x, z := 20, 40
+// 	fmt.Printf("x: %v, %T\n", x, x)
+// 	teste, erro := fmt.Printf("z: %v, %T\n", z, z)
+// 	fmt.Println(teste, erro)
+// }
+
+// Aula2.4
+// var y = 10
+
+// func main() {
+// 	z := 20
+// 	qualquerCoisa(z)
+// }
+// func qualquerCoisa(x int) {
+// 	fmt.Println(y)
+// 	fmt.Println(x)
+// }
