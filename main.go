b457cb15904eff18819701f18b2df5dfd96a035c
celsovcsantos <celsovcsantos@gmail.com>
2024-04-03 13:09:47 -0300

    feat: aula2.3

--- main.go
+++ main.go
@@ -3,7 +3,21 @@ package main
 import "fmt"
 
 // Aula2.2
+// func main() {
+// 	numeroDeBytes, erro := fmt.Println("Hello, World", "teste", 100)
+// 	fmt.Println(numeroDeBytes, erro)
+// }
+
+// Aula2.3
+var y = "Olá, bom dia!"
+
 func main() {
-	numeroDeBytes, erro := fmt.Println("Hello, World", "teste", 100)
-	fmt.Println(numeroDeBytes, erro)
+	x := 10.0
+
+	fmt.Printf("x: %v, %T\n", x, x)
+	fmt.Printf("y: %v, %T\n", y, y)
+	x, z := 20, 40
+	fmt.Printf("x: %v, %T\n", x, x)
+	teste, erro := fmt.Printf("z: %v, %T\n", z, z)
+	fmt.Println(teste, erro)
 }
